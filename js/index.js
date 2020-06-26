a307500b276408faa54de256462521fbf28a5d1c
NorthUniverse <dennis.roshan@gmail.com>
2020-06-25 23:52:09 -0400

    code so far

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
-const banner = document.querySelector('.banner')
+const menuLinks = document.querySelector('.link-wrapper');
+window.onload = function() {
+    menuLinks.classList.add('hide-element');
+};
+
+const banner = document.querySelector('.banner');
 banner.addEventListener('mouseover', () => {
     banner.classList.add('cursor-pointer')
 })
 banner.addEventListener('mouseout', () => {
     banner.classList.remove('cursor-pointer')
-})
+});
 
-const menuBtn = document.querySelector('.menu-btn')
+const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        menuLinks.classList.remove('hide-element');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        menuLinks.classList.add('hide-element');
         menuOpen = false;
     }
 });
