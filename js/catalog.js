d77e7285a617890a74c87efa2bf0da7d7aac6d0e
NorthUniverse <dennis.roshan@gmail.com>
2020-06-30 22:32:25 -0400

    catalog mobile page

---
+++ js/catalog.js
@@ -0,0 +1,7 @@
+const banner = document.querySelector('.banner')
+banner.addEventListener('mouseover', () => {
+    banner.classList.add('cursor-pointer')
+})
+banner.addEventListener('mouseout', () => {
+    banner.classList.remove('cursor-pointer')
+})
