40e04f8759b8cb53623b4b248d2abc344089fdf0
NorthUniverse <dennis.roshan@gmail.com>
2020-07-06 19:45:06 -0400

    abuot styles

--- js/about.js
+++ js/about.js
@@ -5,12 +5,3 @@ banner.addEventListener('mouseover', () => {
 banner.addEventListener('mouseout', () => {
     banner.classList.remove('cursor-pointer')
 });
-
-const aboutImageDiv = document.querySelector('.body-about-background-image');
-const aboutTextDiv = document.querySelector('.body-about-text');
-aboutTextDiv.addEventListener('mouseover', () => {
-    aboutImageDiv.classList.add('about-image-lighten');
-});
-aboutTextDiv.addEventListener('mouseout', () => {
-    aboutImageDiv.classList.remove('about-image-lighten');
-});
